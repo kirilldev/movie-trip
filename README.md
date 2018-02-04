30f8ab0b5cf539e52edc216980ed9ef4b4f6ba0f
Kirill Reznykov <kreznykov@gmail.com>
2018-02-04 19:16:50 +0200

    add read me draft. prettify Main.js

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-# Movie Trip WebApp
+## Movie Trip WebApp
 
 https://movie-trip.herokuapp.com/
+
+#### Description of application core idea
+
+Let's say you are a cinemaddict and you have some spare time
+in the San-Francisco city which you decided to spend visiting some
+places from your favourite movies and to take some photos of it.
+
+MovieTrip App will help you efficiently find those places and plan
+you route using GoogleMaps *which you can then share
+with a friend or send it to your email address.**
+
+**Note: that feature is coming soon :)*
+
+#### Reasoning behind a choice of technologies
+
+After I got that challenge,
+I tried to estimate an amount of time which I could dedicate to that project.
+I found out that I couldn't spend more than 14 hours during those 7 days.
+So as it is not a lot of time to learn something completely new
+I decided to use a stack with which I had a bigger amount of experience
+so I had higher probability to complete a project in time.
+
+**Google Maps API** - I have never tried to use Map APIs before.
+I chose google maps because it has good documentation and
+it is the most popular maps in the world I think
+(which means that they should be more familiar to the end user)
+
+**AngularJS** - I needed some framework to write my own components.
+I was thinking between AngularJS and React. If I had slightly more
+time I would use React. Because AngularJS is older and as a result
+we don't receive new third party components or bug fixes to existing.
+Also React has a performance margin compare to the AngularJS.
+
+**Webpack 3** - The most popular tool. Again,
+I chose boring tech to avoid surprises during implementation.
+
+**Node.js + Express.js** - Node.js allows us
+to reuse some code on a client and a server and it is extremely easy
+to find a hosting for it. Also it fits out needs
+(mostly to serve static resources).
+It minimizes amount of languages in a project.
+If we need some heavy computations or smth else we will
+write micro service using a language which is suitable for that.
+
+#### Trade-offs
+Look for **TODO**s in the source code, it contains some ideas what may be improved.
+
+I decided that at current moment as we don't need to store
+any info on our service we can live without a **DB**.
+
+Also I didn't think seriously about the case when we will
+have much more movies and locations on a map.
+But is should be relatively easy to add it to the existing project.
--- server/Main.js
+++ server/Main.js
@@ -1,4 +1,8 @@
+// Avoid problems with a path if we
+// run script from other directory
 process.chdir(__dirname);
+
+global.SERVER_ENV = process.argv[2] === 'prod' ? 'prod' : 'dev';
 
 const express = require('express');
 const path = require('path');
@@ -8,49 +12,49 @@ const csv = require('csv');
 const parse = require('csv-parse/lib/sync');
 const app = express();
 const GeocodePlaces = require('./GeocodePlaces.js');
+const logger = log4js.getLogger('Main.js');
+const PORT = process.env.PORT || 8090;
+const IP = SERVER_ENV === 'dev' ? 'localhost' : '0.0.0.0';
 
+// TODO: Improve logging, I just added a library..
+// For example. Write error logs to a separate file,
+// Take line number and a file name automatically. etc.
 log4js.configure({
     appenders: {console: {type: 'console'}},
     categories: {default: {appenders: ['console'], level: 'debug'}}
 });
 
-const LOGGER = log4js.getLogger('Main.js');
-const PORT = process.env.PORT || 8090;
-
-global.SERVER_ENV = process.argv[2] === 'prod' ? 'prod' : 'dev';
-
-LOGGER.info(`Going to start server with SERVER_ENV '${global.SERVER_ENV}'`);
-
-const IP = SERVER_ENV === 'dev' ? 'localhost' : '0.0.0.0';
-
-// fs.readFile('./../Film_Locations_in_San_Francisco.csv', 'utf8', function (err, contents) {
-//     const [header, ...rows] = parse(contents);
-//     const places = Array.from(new Set(rows.map(e => e[2])));
-//     const placeNameToCoordinate = {};
-//
-//     places.reduce(mapNameToCoordinate, Promise.resolve(true)).then(writeToAFile);
-//
-//     function writeToAFile() {
-//         const fs = require('fs');
-//         fs.writeFileSync('./../Coordinates_in_San_Francisco.csv', JSON.stringify(placeNameToCoordinate));
-//     }
-//
-//     function mapNameToCoordinate(prevPromise, placeName) {
-//         return prevPromise.then(_ => GeocodePlaces.getCoordinatesByName(placeName)
-//             .then(coordinate => placeNameToCoordinate[placeName] = coordinate));
-//     }
-//
-// });
-
+logger.info(`Going to start server with SERVER_ENV '${SERVER_ENV}'`);
 
 app.disable('x-powered-by');
 
+// static routes
 app.use('/', express.static(path.join(__dirname, '../dist'), { index: 'index.html' }));
 app.use('assets', express.static(path.join(__dirname, '../dist/assets')));
+
+// apis
 app.get('/health', (req, res) => res.status(200).send('OK'));
+
 app.get('/api/heatmap', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.sendFile(path.resolve(__dirname, 'db/Coordinates_in_San_Francisco.csv'))
 });
 
-app.listen(PORT, IP, _ => LOGGER.info(`App ${process.pid} listening on port ${PORT}!`));
+app.get('/api/dataset', (req, res) => {
+    // fs.readFile('./../Film_Locations_in_San_Francisco.csv', 'utf8', function (err, contents) {
+    //     const [header, ...rows] = parse(contents);
+    //     const places = Array.from(new Set(rows.map(e => e[2])));
+    //     const placeNameToCoordinate = {};
+    //
+    //     places.reduce(mapNameToCoordinate, Promise.resolve(true)).then(writeToAFile);
+    //
+    //     function mapNameToCoordinate(prevPromise, placeName) {
+    //         return prevPromise.then(_ => GeocodePlaces.getCoordinatesByName(placeName)
+    //             .then(coordinate => placeNameToCoordinate[placeName] = coordinate));
+    //     }
+    //
+    // });
+});
+
+// start server
+app.listen(PORT, IP, _ => logger.info(`App (PID ${process.pid}) listening on a port: ${PORT}!`));
